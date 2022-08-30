057ca730a58421f24be80d798f323edd61152789
Andrea Miglietta <m.andrea96@hotmail.it>
2022-08-30 21:59:41 +0200

    shopping list in HTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log('JS OK!!!')
+
+// shopping list
+const shoppingList = ['Pere', 'Mele', 'Banane', 'Prugne', 'Limone', 'Cereali', 'Lamponi', 'Arachidi', 'Mais']
+
+// variables
+let list = '';
+let i = 0;
+
+// shopping list while loop
+while (i < shoppingList.length) {
+    list += '<br>Food: ' + shoppingList[i];
+    i++;
+    console.log(shoppingList[i]);
+}
+
+// returns the HTML content (inner.HTML)
+document.getElementById('container').innerHTML = list;
